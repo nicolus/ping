5521e27450256b58f6cbe0cd04255fb0a6eb9d7c
nicolus <bluescreenjunky@hotmail.com>
2023-02-19 09:02:38 +0100

    Validate Phone number

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -9,7 +9,7 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'phone_number' => ['nullable', 'string'],
+            'phone_number' => ['nullable', 'phone'],
             'fcm_token' => ['nullable', 'string']
         ];
     }
