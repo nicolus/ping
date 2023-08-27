96dafaf3302282499600074fbda50188a4e17b06
nicolus <bluescreenjunky@hotmail.com>
2023-08-27 19:07:25 +0200

    Extract MobilLoginRequest validation

--- app/Http/Controllers/AuthenticateMobileApp.php
+++ app/Http/Controllers/AuthenticateMobileApp.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\MobileLoginRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
@@ -13,18 +13,11 @@ class AuthenticateMobileApp extends Controller
     /**
      * Return an authentication token for the mobile app
      *
-     * @param Request $request
-     * @return string
-     * @throws ValidationException
+     * @param MobileLoginRequest $request
+     * @return string a newly created token
      */
-    public function __invoke(Request $request): string
+    public function __invoke(MobileLoginRequest $request): string
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-            'device_name' => 'required',
-        ]);
-
         $user = User::where('email', $request->input('email'))->first();
 
         if (! $user || ! Hash::check($request->input('password'), $user->password)) {
