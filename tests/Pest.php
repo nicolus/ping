728fe1906a908484dafdf8ffc3a55526a6898427
nicolus <bluescreenjunky@hotmail.com>
2023-09-02 08:49:50 +0200

    Don't refresh database for every single test

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,9 +11,7 @@
 |
 */
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-
-uses(Tests\TestCase::class, RefreshDatabase::class)->in('Feature', 'Unit');
+uses(Tests\TestCase::class)->in('Feature', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
