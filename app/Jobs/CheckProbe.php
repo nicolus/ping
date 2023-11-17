c60ffab5e02c9421c51b5fc14d9af14e988babf8
nicolus <bluescreenjunky@gmail.com>
2023-11-17 18:05:22 +0100

    Make CheckProbe job unique

--- app/Jobs/CheckProbe.php
+++ app/Jobs/CheckProbe.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class CheckProbe implements ShouldQueue //, ShouldBeUnique
+class CheckProbe implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
