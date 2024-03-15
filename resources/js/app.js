fe13b132354a7b14be8a458c99c266af732969c1
nicolus <bluescreenjunky@gmail.com>
2024-03-15 18:26:35 +0100

    Upgrade to Laravel 11 + Reverb

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,9 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+import './bootstrap';
 import Toaster from "./toasts";
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine;
 Alpine.start()
-
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: import.meta.env.VITE_PUSHER_SCHEME === 'https',
-    enabledTransports: ["ws", "wss"],
-});
-
 
 window.Echo.private(`App.Models.User.1`).notification((e) => {
    if(e.title && e.text) {
